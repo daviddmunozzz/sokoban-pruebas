af22e900933463a93416992dfbeb39cdddbfadd6
David <david.munoz19@alu.uclm.es>
2024-11-16 23:55:42 +0100

    Tarea 3

--- src/node.py
+++ src/node.py
@@ -1,11 +1,14 @@
 class Node:
-    def __init__(self, ID, parent, state, value, depth, cost, heuristic, action):
-        self.ID = ID
-        self.parent = parent
+    def __init__(self, node_ID, state, parent, action:float, depth, cost:float, heuristic:float, value:int):
+        self.node_ID = node_ID
         self.state = state
+        self.parent = parent
         self.action = action
         self.depth = depth
         self.cost = cost
         self.heuristic = heuristic
         self.value = value
 
+    def __eq__(self, other) -> bool:
+        return self.state == other.state
+    
